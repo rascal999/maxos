2ecd64f201278686b728e8a1109cf6239d3bc976
Your Name <you@example.com>
2022-05-10 18:14:15 +0100

    i3

--- common/i3.nix
+++ common/i3.nix
@@ -32,12 +32,12 @@ in {
         "${mod}+Shift+m" = "move workspace to output DP-5";
       };
 
-      bars = [
-        {
-          position = "bottom";
-          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${./i3status-rust.toml}";
-        }
-      ];
+      #bars = [
+      #  {
+      #    position = "bottom";
+      #    statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${./i3status-rust.toml}";
+      #  }
+      #];
     };
   };
 }
