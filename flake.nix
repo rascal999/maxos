6d8cb40534005d15c2271a7300b8ec05e2af717a
Aidan Marlin <spam@gmail.com>
2022-05-12 10:34:11 +0100

    flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         system = "x86_64-linux";
         modules = [
           ./shared.nix
-          ./hosts/test/test.nix
+          ./hosts/vm-rog-test/vm-rog-test.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
