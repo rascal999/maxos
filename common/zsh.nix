9351c3e9b7db6c82047b5d8250a1938993fe9105
Aidan Marlin <spam@gmail.com>
2022-05-12 15:56:07 +0100

    p10k

--- common/zsh.nix
+++ common/zsh.nix
@@ -18,11 +18,11 @@
     initExtra = builtins.readFile ../configs/zshrc.zsh;
 
     plugins = with pkgs; [
-      #{
-      #  file = "powerlevel10k.zsh-theme";
-      #  name = "powerlevel10k";
-      #  src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
-      #}
+      {
+        file = "powerlevel10k.zsh-theme";
+        name = "powerlevel10k";
+        src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
+      }
       {
         file = "p10k.zsh";
         name = "powerlevel10k-config";
