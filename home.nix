8ddf6d76fe476dabfebf37fd6dcafde7c609c8ef
Aidan Marlin <spam@gmail.com>
2022-05-12 11:09:58 +0100

    tmux

--- home.nix
+++ home.nix
@@ -24,6 +24,24 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  # tmux
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      bind "Enter" send-keys "ls -alh" \; send-keys "Enter"
+      bind "Space" send-keys "ls -alhtr" \; send-keys "Enter"
+      bind "e" send-keys "exit" \; send-keys "Enter"
+      bind l send-keys "df -h" \; send-keys "Enter"
+      bind r send-keys "uname -a" \; send-keys "Enter"
+      bind u send-keys "du -sh ." \; send-keys "Enter"
+
+      unbind C-b
+      unbind [
+      unbind ]
+
+      set -g prefix C-Space'';
+  };
+
   # Zsh
   programs.zsh = {
     dirHashes = {
