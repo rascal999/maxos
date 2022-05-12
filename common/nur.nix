e1af378873a4cd19902177fa50ee00d7951d43d7
Aidan Marlin <spam@gmail.com>
2022-05-12 12:26:22 +0100

    nur

--- common/nur.nix
+++ common/nur.nix
@@ -2,8 +2,8 @@
 
   nixpkgs.config.packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball {
-      url = "https://github.com/nix-community/NUR/commit/f69cfc11e9b2312a294b8a6291de721d0d207819";
-      sha256 = "f69cfc11e9b2312a294b8a6291de721d0d207819";
+      url = "https://github.com/nix-community/NUR/archive/f69cfc11e9b2312a294b8a6291de721d0d207819.zip";
+      sha256 = "1hgawnbg8ahcck38bxlw2d1lsymazyrng3yfpqrxpsz5l1pijrsy";
     }) {
       inherit pkgs;
     };
