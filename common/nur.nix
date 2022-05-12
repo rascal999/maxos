7c743beddee0a2cbeb05083ddc9e8db04366bd08
Aidan Marlin <spam@gmail.com>
2022-05-12 12:23:43 +0100

    nur

--- common/nur.nix
+++ common/nur.nix
@@ -4,7 +4,7 @@
     nur = import (builtins.fetchTarball {
       url = "https://github.com/nix-community/NUR/commit/f69cfc11e9b2312a294b8a6291de721d0d207819";
       sha256 = "f69cfc11e9b2312a294b8a6291de721d0d207819";
-    ) {
+    }) {
       inherit pkgs;
     };
   };
