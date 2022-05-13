da9b64e378256f36e442a2a292eb188b069c5448
Aidan Marlin <spam@gmail.com>
2022-05-13 11:06:47 +0100

    refactor

--- README.md
+++ README.md
@@ -4,9 +4,23 @@
 
 Use unstable ISO (https://www.google.com/search?q=nixos+iso+unstable) for flake support.
 
+### Get repo
+
+```
+Set SSH key for GitHub
+git clone git@github.com:rascal999/nixos.git
+```
+
+OR
+
 ```
 curl -L https://bit.ly/3yCeldM -o master.zip
-unzip master.zip
-cd nixos-master/
+unzip master.zip -d nixos
+```
+
+### Install
+
+```
+cd nixos/
 ./init.sh
 ```
--- setup.sh
+++ setup.sh
@@ -8,12 +8,15 @@ fi
 nixos-generate-config --root /mnt
 rm /mnt/etc/nixos/configuration.nix
 
-mkdir hosts/$1
-cp /mnt/etc/nixos/hardware.nix hosts/$1/${1}.nix
-nix-env -i git
-git add .
-git commit -m "Host $1"
-git push
+if [[ -d ".git" ]]; then
+  mkdir hosts/$1
+  cp /mnt/etc/nixos/hardware.nix hosts/$1/${1}.nix
+  nix-env -i git
+  git add .
+  git commit -m "Host $1"
+  git push
+fi
+
 exit 1
 
 nixos-install --flake .#$1 --root /mnt --impure
