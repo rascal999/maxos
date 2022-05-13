bf0fad9ebf470b142a9ee0a24cc789193722c4fe
Aidan Marlin <spam@gmail.com>
2022-05-13 11:39:29 +0100

    xresources

--- common/xresources.nix
+++ common/xresources.nix
@@ -3,7 +3,7 @@
 {
   xresources.properties = {
     "urxvt.scrollBar" = "false";
-    #"urxvt.font" = "xft = MesloLGS Nerd Font Mono = pixelsize=22";
+    "urxvt.font" = "xft = MesloLGS Nerd Font = pixelsize=22";
     "*background" = "#101010";
     "*foreground" = "#66FF66";
     "*cursorColor" = "#66ff66";
