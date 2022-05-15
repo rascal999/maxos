e5953f23d8af4a0ccdd4d99132882b102798ed29
Aidan Marlin <spam@gmail.com>
2022-05-15 17:10:41 +0100

    rog host

--- hosts/rog/rog-hw.nix
+++ hosts/rog/rog-hw.nix
@@ -8,24 +8,32 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/210dd1cb-8c15-4e10-9632-fc9c88e42d69";
+    { device = "/dev/disk/by-uuid/19b028e0-b8e2-47b2-a73a-612a36247293";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/fee65183-7fac-47da-9be8-c8b72111992f";
+  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/9344b55a-a499-449e-8212-2dc43771bd70";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/EE91-FC44";
+    { device = "/dev/disk/by-uuid/287D-9F3E";
       fsType = "vfat";
     };
 
   swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp4s0f3u1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
