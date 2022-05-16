9eb2377a39f326d07914b9ec9b8e90c9230d5348
Aidan Marlin <spam@gmail.com>
2022-05-16 12:26:47 +0100

    zsh

--- common/zsh.nix
+++ common/zsh.nix
@@ -19,17 +19,17 @@
       source ${./p10k.zsh}
     '';
 
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "zsh-autosuggestions" "zsh-syntax-highlighting" ]
+    }
+
     plugins = with pkgs; [
       {
         file = "powerlevel10k.zsh-theme";
         name = "powerlevel10k";
         src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
       }
-      #{
-      #  file = "p10k.zsh";
-      #  name = "powerlevel10k-config";
-      #  src = ./p10k.zsh;
-      #}
     ];
 
     shellAliases = {
