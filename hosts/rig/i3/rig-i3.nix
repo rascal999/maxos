7c87dbb8bf06cc3d94350e4c20b6a411e79bf97b
Aidan Marlin <spam@gmail.com>
2022-05-17 09:02:01 +0100

    rig host

--- hosts/rig/i3/rig-i3.nix
+++ hosts/rig/i3/rig-i3.nix
@@ -72,6 +72,9 @@ in {
         {
           command = "exec ${../../../scripts/monitors.sh} ${./rig-workspace-1.json} ${./rig-workspace-2.json} ${./rig-workspace-7.json}";
         }
+        {
+          command = "exec ${pkgs.nvidia-settings}/bin/nvidia-settings -a GPUFanControlState=1 -a GPUTargetFanSpeed=40"
+        }
       ];
 
       bars = [
