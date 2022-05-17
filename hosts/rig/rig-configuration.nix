12c2909ce2aa65fea74816435d8588e49013adfb
Aidan Marlin <spam@gmail.com>
2022-05-17 10:00:03 +0100

    rig host

--- hosts/rig/rig-configuration.nix
+++ hosts/rig/rig-configuration.nix
@@ -5,4 +5,13 @@
   networking.hostName = "rig";
   services.xserver.videoDrivers = [ "nvidia" ];
   virtualisation.docker.enableNvidia = true;
+
+  # Enable cron service
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "* * * * *        user    ${pkgs.rsync}/bin/rsync -avz --no-perms /home/user/Data/ admin@192.168.0.254:/volume1/k8s-syncthing-data/data/"
+      "* * * * *        user    ${pkgs.rsync}/bin/rsync -avz --no-perms /home/user/Camera/Camera admin@192.168.0.254:/volume1/k8s-syncthing-data/camera/"
+    ];
+  };
 }
