d50df2dac2af48e40065eb1a5633dc728ce746dc
Aidan Marlin <spam@gmail.com>
2022-05-18 08:51:22 +0100

    shared.nix

--- shared.nix
+++ shared.nix
@@ -352,6 +352,7 @@
 
   ## VirtualBox
   virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
   users.extraGroups.vboxusers.members = [ "user" ];
 
   # opensnitch
