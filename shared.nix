9d65a3d4218f75b5c3526c4dfc71ab352e08b52f
Aidan Marlin <spam@gmail.com>
2022-05-23 13:18:43 +0100

    shared.nix

--- shared.nix
+++ shared.nix
@@ -8,6 +8,7 @@ with pkgs;
 let
   my-python-packages = python-packages: with python-packages; [
     pip
+    setuptools
   ]; 
   python-with-my-packages = python3.withPackages my-python-packages;
 in {
