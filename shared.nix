cf87e21dc59796898e0c6a088706608e8bac72a9
Aidan Marlin <spam@gmail.com>
2022-06-05 17:08:04 +0100

    shared.nix jupyter

--- shared.nix
+++ shared.nix
@@ -384,6 +384,7 @@ in {
   services.jupyter = {
     enable = true;
     notebookDir = "~/jupyter";
+    password = "'sha1:1b961dc713fb:88483270a63e57d18d43cf337e629539de1436ba'";
   };
 
   users.users.jupyter.group = "jupyter";
