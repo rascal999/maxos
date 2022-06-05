5f5cf3fe786ba456ed4cdc8410a771009bef2894
Aidan Marlin <spam@gmail.com>
2022-06-05 18:25:16 +0100

    shared.nix jupyter

--- shared.nix
+++ shared.nix
@@ -384,7 +384,6 @@ in {
   services.jupyter = {
     enable = true;
     password = "'sha1:1b961dc713fb:88483270a63e57d18d43cf337e629539de1436ba'";
-    command = "PATH=\"$PATH:/run/current-system/sw/bin\"";
   };
 
   users.users.jupyter.group = "jupyter";
