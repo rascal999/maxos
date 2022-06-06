14224ce9b26a38c254b45ebb4cf59472c68847a2
Aidan Marlin <spam@gmail.com>
2022-06-06 11:46:57 +0100

    firefox screenshot profile

--- common/firefox/firefox.nix
+++ common/firefox/firefox.nix
@@ -75,7 +75,7 @@
       default = {
         id = 0;
         name = "Default";
-        isDefault = true;
+        isDefault = false;
         settings = defaultSettings;
 
         userChrome = import ./css/userChrome.css;
@@ -95,7 +95,7 @@
       screenshot = {
         id = 3;
         name = "Screenshot";
-        isDefault = false;
+        isDefault = true;
         settings = defaultSettings;
 
         userChrome = import ./css/userChrome.css;
