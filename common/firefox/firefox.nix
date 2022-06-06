7058ba600a07084875207e7b7c2e60c52824e091
Aidan Marlin <spam@gmail.com>
2022-06-06 11:34:10 +0100

    firefox screenshot profile

--- common/firefox/firefox.nix
+++ common/firefox/firefox.nix
@@ -2,232 +2,235 @@
   # Firefox
   programs.firefox = {
     enable = true;
-    profiles.default = {
-      id = 0;
-      name = "Default";
-      isDefault = true;
-      settings = { 
-        "app.update.auto" = false;
 
-        "browser.aboutConfig.showWarning" = false;
-        "browser.crashReports.unsubmittedCheck.enabled" = false;
-        "browser.newtabpage.activity-stream.enabled" = false;
-        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-        "browser.newtabpage.activity-stream.telemetry" = false;
-        "browser.ping-centre.telemetry" = false;
-        "browser.search.openintab" = true;
-        "browser.send_pings" = false;
-        "browser.toolbars.bookmarks.visibility" = "always";
+    profiles = {
+      default = {
+        id = 0;
+        name = "Default";
+        isDefault = true;
+        settings = { 
+          "app.update.auto" = false;
 
-        "datareporting.policy.dataSubmissionEnabled" = false;
+          "browser.aboutConfig.showWarning" = false;
+          "browser.crashReports.unsubmittedCheck.enabled" = false;
+          "browser.newtabpage.activity-stream.enabled" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "browser.search.openintab" = true;
+          "browser.send_pings" = false;
+          "browser.toolbars.bookmarks.visibility" = "always";
 
-        "device.sensors.enabled" = false;
+          "datareporting.policy.dataSubmissionEnabled" = false;
 
-        "devtools.chrome.enabled" = true;
-        "devtools.debugger.remote-enabled" = false;
-        "devtools.inspector.showUserAgentStyles" = true;
-        "devtools.theme" = "dark";
+          "device.sensors.enabled" = false;
 
-        "extensions.autoDisableScopes" = "0";
+          "devtools.chrome.enabled" = true;
+          "devtools.debugger.remote-enabled" = false;
+          "devtools.inspector.showUserAgentStyles" = true;
+          "devtools.theme" = "dark";
 
-        # Disable pocket
-        "extensions.pocket.enable" = false;
+          "extensions.autoDisableScopes" = "0";
 
-        # disable all the autofill prompts
-        "extensions.formautofill.addresses.enabled" = false;
-        "extensions.formautofill.creditCards.enabled" = false;
+          # Disable pocket
+          "extensions.pocket.enable" = false;
 
-        "experiments.enabled" = false;
-        "experiments.supported" = false;
+          # disable all the autofill prompts
+          "extensions.formautofill.addresses.enabled" = false;
+          "extensions.formautofill.creditCards.enabled" = false;
 
-        # Because I'm blind
-        "layout.css.devPixelsPerPx" = "1.3";
+          "experiments.enabled" = false;
+          "experiments.supported" = false;
 
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.trackingprotection.cryptomining.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
-        "privacy.trackingprotection.pbmode.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
+          # Because I'm blind
+          "layout.css.devPixelsPerPx" = "1.3";
 
-        "services.sync.engine.addons" = false;
-        "services.sync.engine.passwords" = false;
-        "services.sync.engine.prefs" = false;
-        "services.sync.engineStatusChanged.addons" = true;
+          "privacy.donottrackheader.enabled" = true;
+          "privacy.trackingprotection.cryptomining.enabled" = true;
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.fingerprinting.enabled" = true;
+          "privacy.trackingprotection.pbmode.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
 
-        "signon.rememberSignons" = false;
+          "services.sync.engine.addons" = false;
+          "services.sync.engine.passwords" = false;
+          "services.sync.engine.prefs" = false;
+          "services.sync.engineStatusChanged.addons" = true;
 
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "signon.rememberSignons" = false;
 
-        "toolkit.telemetry.archive.enabled" = false;
-        "toolkit.telemetry.bhrPing.enabled" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.firstShutdownPing.enabled" = false;
-        "toolkit.telemetry.hybridContent.enabled" = false;
-        "toolkit.telemetry.newProfilePing.enabled" = false;
-        "toolkit.telemetry.reportingpolicy.firstRun" = false;
-        "toolkit.telemetry.server" = "";
-        "toolkit.telemetry.shutdownPingSender.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.updatePing.enabled" = false;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
 
-        "ui.systemUsesDarkTheme" = 1;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.server" = "";
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
+
+          "ui.systemUsesDarkTheme" = 1;
+        };
+
+        userChrome = import ./css/userChrome.css;
+        userContent = import ./css/userContent.css;
       };
 
-      userChrome = import ./css/userChrome.css;
-      userContent = import ./css/userContent.css;
-    };
+      burp = {
+        id = 1;
+        name = "Burp";
+        isDefault = false;
+        settings = { 
+          "app.update.auto" = false;
 
-    profiles.burp = {
-      id = 1;
-      name = "Burp";
-      isDefault = false;
-      settings = { 
-        "app.update.auto" = false;
+          "browser.aboutConfig.showWarning" = false;
+          "browser.crashReports.unsubmittedCheck.enabled" = false;
+          "browser.newtabpage.activity-stream.enabled" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "browser.search.openintab" = true;
+          "browser.send_pings" = false;
+          "browser.toolbars.bookmarks.visibility" = "always";
 
-        "browser.aboutConfig.showWarning" = false;
-        "browser.crashReports.unsubmittedCheck.enabled" = false;
-        "browser.newtabpage.activity-stream.enabled" = false;
-        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-        "browser.newtabpage.activity-stream.telemetry" = false;
-        "browser.ping-centre.telemetry" = false;
-        "browser.search.openintab" = true;
-        "browser.send_pings" = false;
-        "browser.toolbars.bookmarks.visibility" = "always";
+          "datareporting.policy.dataSubmissionEnabled" = false;
 
-        "datareporting.policy.dataSubmissionEnabled" = false;
+          "device.sensors.enabled" = false;
 
-        "device.sensors.enabled" = false;
+          "devtools.chrome.enabled" = true;
+          "devtools.debugger.remote-enabled" = false;
+          "devtools.inspector.showUserAgentStyles" = true;
+          "devtools.theme" = "dark";
 
-        "devtools.chrome.enabled" = true;
-        "devtools.debugger.remote-enabled" = false;
-        "devtools.inspector.showUserAgentStyles" = true;
-        "devtools.theme" = "dark";
+          "extensions.autoDisableScopes" = "0";
 
-        "extensions.autoDisableScopes" = "0";
+          # Disable pocket
+          "extensions.pocket.enable" = false;
 
-        # Disable pocket
-        "extensions.pocket.enable" = false;
+          # disable all the autofill prompts
+          "extensions.formautofill.addresses.enabled" = false;
+          "extensions.formautofill.creditCards.enabled" = false;
 
-        # disable all the autofill prompts
-        "extensions.formautofill.addresses.enabled" = false;
-        "extensions.formautofill.creditCards.enabled" = false;
+          "experiments.enabled" = false;
+          "experiments.supported" = false;
 
-        "experiments.enabled" = false;
-        "experiments.supported" = false;
+          # Because I'm blind
+          "layout.css.devPixelsPerPx" = "1.3";
 
-        # Because I'm blind
-        "layout.css.devPixelsPerPx" = "1.3";
+          "privacy.donottrackheader.enabled" = true;
+          "privacy.trackingprotection.cryptomining.enabled" = true;
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.fingerprinting.enabled" = true;
+          "privacy.trackingprotection.pbmode.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
 
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.trackingprotection.cryptomining.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
-        "privacy.trackingprotection.pbmode.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
+          "services.sync.engine.addons" = false;
+          "services.sync.engine.passwords" = false;
+          "services.sync.engine.prefs" = false;
+          "services.sync.engineStatusChanged.addons" = true;
 
-        "services.sync.engine.addons" = false;
-        "services.sync.engine.passwords" = false;
-        "services.sync.engine.prefs" = false;
-        "services.sync.engineStatusChanged.addons" = true;
+          "signon.rememberSignons" = false;
 
-        "signon.rememberSignons" = false;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
 
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.server" = "";
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
 
-        "toolkit.telemetry.archive.enabled" = false;
-        "toolkit.telemetry.bhrPing.enabled" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.firstShutdownPing.enabled" = false;
-        "toolkit.telemetry.hybridContent.enabled" = false;
-        "toolkit.telemetry.newProfilePing.enabled" = false;
-        "toolkit.telemetry.reportingpolicy.firstRun" = false;
-        "toolkit.telemetry.server" = "";
-        "toolkit.telemetry.shutdownPingSender.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.updatePing.enabled" = false;
+          "ui.systemUsesDarkTheme" = 1;
+        };
 
-        "ui.systemUsesDarkTheme" = 1;
+        userChrome = import ./css/userChromeBurp.css;
+        userContent = import ./css/userContent.css;
       };
 
-      userChrome = import ./css/userChromeBurp.css;
-      userContent = import ./css/userContent.css;
-    };
+      screenshot = {
+        id = 3;
+        name = "Screenshot";
+        isDefault = false;
+        settings = { 
+          "app.update.auto" = false;
 
-    profiles.screenshot = {
-      id = 3;
-      name = "Screenshot";
-      isDefault = false;
-      settings = { 
-        "app.update.auto" = false;
+          "browser.aboutConfig.showWarning" = false;
+          "browser.crashReports.unsubmittedCheck.enabled" = false;
+          "browser.newtabpage.activity-stream.enabled" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "browser.search.openintab" = true;
+          "browser.send_pings" = false;
+          "browser.toolbars.bookmarks.visibility" = "always";
 
-        "browser.aboutConfig.showWarning" = false;
-        "browser.crashReports.unsubmittedCheck.enabled" = false;
-        "browser.newtabpage.activity-stream.enabled" = false;
-        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-        "browser.newtabpage.activity-stream.telemetry" = false;
-        "browser.ping-centre.telemetry" = false;
-        "browser.search.openintab" = true;
-        "browser.send_pings" = false;
-        "browser.toolbars.bookmarks.visibility" = "always";
+          "datareporting.policy.dataSubmissionEnabled" = false;
 
-        "datareporting.policy.dataSubmissionEnabled" = false;
+          "device.sensors.enabled" = false;
 
-        "device.sensors.enabled" = false;
+          "devtools.chrome.enabled" = true;
+          "devtools.debugger.remote-enabled" = false;
+          "devtools.inspector.showUserAgentStyles" = true;
+          "devtools.theme" = "dark";
 
-        "devtools.chrome.enabled" = true;
-        "devtools.debugger.remote-enabled" = false;
-        "devtools.inspector.showUserAgentStyles" = true;
-        "devtools.theme" = "dark";
+          "extensions.autoDisableScopes" = "0";
 
-        "extensions.autoDisableScopes" = "0";
+          # Disable pocket
+          "extensions.pocket.enable" = false;
 
-        # Disable pocket
-        "extensions.pocket.enable" = false;
+          # disable all the autofill prompts
+          "extensions.formautofill.addresses.enabled" = false;
+          "extensions.formautofill.creditCards.enabled" = false;
 
-        # disable all the autofill prompts
-        "extensions.formautofill.addresses.enabled" = false;
-        "extensions.formautofill.creditCards.enabled" = false;
+          "experiments.enabled" = false;
+          "experiments.supported" = false;
 
-        "experiments.enabled" = false;
-        "experiments.supported" = false;
+          # Because I'm blind
+          "layout.css.devPixelsPerPx" = "1.3";
 
-        # Because I'm blind
-        "layout.css.devPixelsPerPx" = "1.3";
+          "privacy.donottrackheader.enabled" = true;
+          "privacy.trackingprotection.cryptomining.enabled" = true;
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.fingerprinting.enabled" = true;
+          "privacy.trackingprotection.pbmode.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
 
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.trackingprotection.cryptomining.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
-        "privacy.trackingprotection.pbmode.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
+          "services.sync.engine.addons" = false;
+          "services.sync.engine.passwords" = false;
+          "services.sync.engine.prefs" = false;
+          "services.sync.engineStatusChanged.addons" = true;
 
-        "services.sync.engine.addons" = false;
-        "services.sync.engine.passwords" = false;
-        "services.sync.engine.prefs" = false;
-        "services.sync.engineStatusChanged.addons" = true;
+          "signon.rememberSignons" = false;
 
-        "signon.rememberSignons" = false;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
 
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.server" = "";
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
 
-        "toolkit.telemetry.archive.enabled" = false;
-        "toolkit.telemetry.bhrPing.enabled" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.firstShutdownPing.enabled" = false;
-        "toolkit.telemetry.hybridContent.enabled" = false;
-        "toolkit.telemetry.newProfilePing.enabled" = false;
-        "toolkit.telemetry.reportingpolicy.firstRun" = false;
-        "toolkit.telemetry.server" = "";
-        "toolkit.telemetry.shutdownPingSender.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.updatePing.enabled" = false;
+          "ui.systemUsesDarkTheme" = 1;
+        };
 
-        "ui.systemUsesDarkTheme" = 1;
+        userChrome = import ./css/userChrome.css;
+        userContent = import ./css/userContent.css;
       };
-
-      userChrome = import ./css/userChrome.css;
-      userContent = import ./css/userContent.css;
     };
   };
 }
