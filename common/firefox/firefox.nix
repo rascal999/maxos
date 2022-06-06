99dcf0fe731d7befba5d2927078d82dd8cea48f9
Aidan Marlin <spam@gmail.com>
2022-06-06 11:45:48 +0100

    firefox screenshot profile

--- common/firefox/firefox.nix
+++ common/firefox/firefox.nix
@@ -1,106 +1,105 @@
 { config, pkgs, lib, ... }: {
-  let defaultSettings = {
-    "app.update.auto" = false;
+  # Firefox
+  programs.firefox = {
+    enable = true;
 
-    "browser.aboutConfig.showWarning" = false;
-    "browser.crashReports.unsubmittedCheck.enabled" = false;
-    "browser.newtabpage.activity-stream.enabled" = false;
-    "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-    "browser.newtabpage.activity-stream.telemetry" = false;
-    "browser.ping-centre.telemetry" = false;
-    "browser.search.openintab" = true;
-    "browser.send_pings" = false;
-    "browser.toolbars.bookmarks.visibility" = "always";
+    profiles =
+      let defaultSettings = {
+        "app.update.auto" = false;
 
-    "datareporting.policy.dataSubmissionEnabled" = false;
+        "browser.aboutConfig.showWarning" = false;
+        "browser.crashReports.unsubmittedCheck.enabled" = false;
+        "browser.newtabpage.activity-stream.enabled" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "browser.ping-centre.telemetry" = false;
+        "browser.search.openintab" = true;
+        "browser.send_pings" = false;
+        "browser.toolbars.bookmarks.visibility" = "always";
 
-    "device.sensors.enabled" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false;
 
-    "devtools.chrome.enabled" = true;
-    "devtools.debugger.remote-enabled" = false;
-    "devtools.inspector.showUserAgentStyles" = true;
-    "devtools.theme" = "dark";
+        "device.sensors.enabled" = false;
 
-    "extensions.autoDisableScopes" = "0";
+        "devtools.chrome.enabled" = true;
+        "devtools.debugger.remote-enabled" = false;
+        "devtools.inspector.showUserAgentStyles" = true;
+        "devtools.theme" = "dark";
 
-    # Disable pocket
-    "extensions.pocket.enable" = false;
+        "extensions.autoDisableScopes" = "0";
 
-    # disable all the autofill prompts
-    "extensions.formautofill.addresses.enabled" = false;
-    "extensions.formautofill.creditCards.enabled" = false;
+        # Disable pocket
+        "extensions.pocket.enable" = false;
 
-    "experiments.enabled" = false;
-    "experiments.supported" = false;
+        # disable all the autofill prompts
+        "extensions.formautofill.addresses.enabled" = false;
+        "extensions.formautofill.creditCards.enabled" = false;
 
-    # Because I'm blind
-    "layout.css.devPixelsPerPx" = "1.3";
+        "experiments.enabled" = false;
+        "experiments.supported" = false;
 
-    "privacy.donottrackheader.enabled" = true;
-    "privacy.trackingprotection.cryptomining.enabled" = true;
-    "privacy.trackingprotection.enabled" = true;
-    "privacy.trackingprotection.fingerprinting.enabled" = true;
-    "privacy.trackingprotection.pbmode.enabled" = true;
-    "privacy.trackingprotection.socialtracking.enabled" = true;
+        # Because I'm blind
+        "layout.css.devPixelsPerPx" = "1.3";
 
-    "services.sync.engine.addons" = false;
-    "services.sync.engine.passwords" = false;
-    "services.sync.engine.prefs" = false;
-    "services.sync.engineStatusChanged.addons" = true;
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.trackingprotection.cryptomining.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.pbmode.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
 
-    "signon.rememberSignons" = false;
+        "services.sync.engine.addons" = false;
+        "services.sync.engine.passwords" = false;
+        "services.sync.engine.prefs" = false;
+        "services.sync.engineStatusChanged.addons" = true;
 
-    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "signon.rememberSignons" = false;
 
-    "toolkit.telemetry.archive.enabled" = false;
-    "toolkit.telemetry.bhrPing.enabled" = false;
-    "toolkit.telemetry.enabled" = false;
-    "toolkit.telemetry.firstShutdownPing.enabled" = false;
-    "toolkit.telemetry.hybridContent.enabled" = false;
-    "toolkit.telemetry.newProfilePing.enabled" = false;
-    "toolkit.telemetry.reportingpolicy.firstRun" = false;
-    "toolkit.telemetry.server" = "";
-    "toolkit.telemetry.shutdownPingSender.enabled" = false;
-    "toolkit.telemetry.unified" = false;
-    "toolkit.telemetry.updatePing.enabled" = false;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
 
-    "ui.systemUsesDarkTheme" = 1;
-  };
-  in {
-    # Firefox
-    programs.firefox = {
-      enable = true;
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.hybridContent.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.reportingpolicy.firstRun" = false;
+        "toolkit.telemetry.server" = "";
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
 
-      profiles = {
-        default = {
-          id = 0;
-          name = "Default";
-          isDefault = true;
-          settings = defaultSettings;
+        "ui.systemUsesDarkTheme" = 1;
+      };
+    in {
+      default = {
+        id = 0;
+        name = "Default";
+        isDefault = true;
+        settings = defaultSettings;
 
-          userChrome = import ./css/userChrome.css;
-          userContent = import ./css/userContent.css;
-        };
+        userChrome = import ./css/userChrome.css;
+        userContent = import ./css/userContent.css;
+      };
 
-        burp = {
-          id = 1;
-          name = "Burp";
-          isDefault = false;
-          settings = defaultSettings;
+      burp = {
+        id = 1;
+        name = "Burp";
+        isDefault = false;
+        settings = defaultSettings;
 
-          userChrome = import ./css/userChromeBurp.css;
-          userContent = import ./css/userContent.css;
-        };
+        userChrome = import ./css/userChromeBurp.css;
+        userContent = import ./css/userContent.css;
+      };
 
-        screenshot = {
-          id = 3;
-          name = "Screenshot";
-          isDefault = false;
-          settings = defaultSettings;
+      screenshot = {
+        id = 3;
+        name = "Screenshot";
+        isDefault = false;
+        settings = defaultSettings;
 
-          userChrome = import ./css/userChrome.css;
-          userContent = import ./css/userContent.css;
-        };
+        userChrome = import ./css/userChrome.css;
+        userContent = import ./css/userContent.css;
       };
     };
   };
