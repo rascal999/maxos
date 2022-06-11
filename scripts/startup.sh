64a80b706731b7f33e34c0c8bcf91594361e65a1
Aidan Marlin <spam@gmail.com>
2022-06-11 12:08:48 +0100

    flake.nix ISO

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 git clone https://github.com/rascal999/nixos ${HOME}/git/nixos || git -C ${HOME}/git/nixos pull
 mkdir -p ${HOME}/jupyter/pentest/base
 rm ${HOME}/jupyter/pentest/base/*.ipynb
