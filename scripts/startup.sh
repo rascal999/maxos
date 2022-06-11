7527491d338db81de75de63d8c82b21eb2de8a66
Aidan Marlin <spam@gmail.com>
2022-06-11 12:10:02 +0100

    flake.nix ISO

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/rascal999/nixos ${HOME}/git/nixos || git -C ${HOME}/git/nixos pull
+${pkgs.git}/bin/git clone https://github.com/rascal999/nixos ${HOME}/git/nixos || ${pkgs.git}/bin/git -C ${HOME}/git/nixos pull
 mkdir -p ${HOME}/jupyter/pentest/base
 rm ${HOME}/jupyter/pentest/base/*.ipynb
 cp ${HOME}/git/nixos/resources/jupyter/pentest/*.ipynb ${HOME}/jupyter/pentest/base
