c6788d6277edd6b41e1ac2ac98453074bd9ff2ff
Aidan Marlin <spam@gmail.com>
2022-06-11 10:34:30 +0100

    flake.nix ISO

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -28,5 +28,12 @@
 
   # Override defaults
   fonts.fontconfig.enable = lib.mkForce true;
-  services.xserver.resolutions = lib.mkOverride [{ x = 1920; y = 1440; } { x = 1024; y = 768 }];
+  services.xserver.resolutions = lib.mkOverride [
+    {
+      x = 1920; y = 1440;
+    }
+    {
+      x = 1024; y = 768;
+    }
+  ];
 }
