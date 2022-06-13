e5e754632a0631408552568e5b02e9a8770b9070
Aidan Marlin <spam@gmail.com>
2022-06-13 10:15:07 +0100

    tmux.nix changes

--- common/tmux.nix
+++ common/tmux.nix
@@ -8,9 +8,9 @@
       bind "Enter" send-keys "ls -alh" \; send-keys "Enter"
       bind "Space" send-keys "ls -alhtr" \; send-keys "Enter"
       bind "e" send-keys "exit" \; send-keys "Enter"
-      bind l send-keys "df -h" \; send-keys "Enter"
+      bind l send-keys "duf" \; send-keys "Enter"
       bind r send-keys "uname -a" \; send-keys "Enter"
-      bind u send-keys "du -sh ." \; send-keys "Enter"
+      bind u send-keys "dust ." \; send-keys "Enter"
 
       unbind C-b
       unbind [
