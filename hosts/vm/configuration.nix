4fce2137ff4f0cb8a4b3714937626b54091a1ebe
Aidan Marlin <spam@gmail.com>
2022-06-13 16:20:26 +0100

    NetworkManager for ISO

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -2,12 +2,9 @@
 
 {
   networking.hostName = "iso";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.networkmanager.enable = true;
-  networking.networkmanager.wifi.scanRandMacAddress = false;
+  # To allow networkmanager
+  networking.wireless.enable = lib.mkForce false;
 
   # X11 / i3
   services.xserver = {
