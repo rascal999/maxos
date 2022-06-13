b6c377758d5e4b9017b107651e154a0a4852289c
Aidan Marlin <spam@gmail.com>
2022-06-13 14:30:26 +0100

    Refactor

--- hosts/rog/configuration.nix
+++ hosts/rog/configuration.nix
@@ -10,59 +10,11 @@ let
   '';
 in
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.grub.useOSProber = true;
-
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
-  boot.loader.grub.extraEntries = ''
-    menuentry "Reboot" {
-      reboot
-    }
-    menuentry "Poweroff" {
-      halt
-    }
-  '';
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.networkmanager.enable = true;
-  networking.networkmanager.wifi.scanRandMacAddress = false;
-
   networking.hostName = "rog";
   hardware.nvidia.modesetting.enable = true;
   hardware.opengl.enable = true;
 
   environment.systemPackages = [ nvidia-offload ];
-
-  # X11 / i3
-  services.xserver = {
-    enable = true;
-    windowManager.i3.enable = true;
-
-    displayManager = {
-      defaultSession = "none+i3";
-      lightdm.enable = true;
-      autoLogin.enable = true;
-      autoLogin.user = "user";
-    };
-  };
-
-  services.xserver = {
-    # Touchpad
-    synaptics = {
-      enable = true;
-      vertTwoFingerScroll = true;
-      palmDetect = true;
-      minSpeed = "1.5";
-    };
-  };
 
   services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
   hardware.nvidia.prime = {
