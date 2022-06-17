9d27427fb8238e787a02c4596d215b5e9e44480c
Aidan <spam@gmail.com>
2022-06-17 18:18:44 +0100

    docker changes

--- common/docker.nix
+++ common/docker.nix
@@ -1,10 +1,23 @@
 { config, pkgs, lib, ... }: {
-    virtualisation.oci-containers.backend = "docker";
-    virtualisation.oci-containers.containers = {
-    "focalboard" = {
-      image = "mattermost/focalboard";
-      ports = [ "9010:80" ];
-      volumes = [ "/home/user/Data/focalboard:/data"];
+  virtualisation.oci-containers.backend = "docker";
+  #virtualisation.oci-containers.containers = {
+  #  focalboard = {
+  #    image = "mattermost/focalboard";
+  #    ports = [ "9010:8000" ];
+  #    volumes = [ "/home/user/Data/focalboard:/data"];
+  #  };
+  #};
+  virtualisation.oci-containers.containers = {
+    focalboard = {
+      image = "zadam/trilium";
+      ports = [ "9020:8080" ];
+      volumes = [ "/home/user/Data/trilium:/home/node/trilium-data"];
+    };
+  };
+  virtualisation.oci-containers.containers = {
+    openvas = {
+      image = "mikesplain/openvas";
+      ports = [ "8030:443" ];
     };
   };
 }
