c8bd8834f50204b39417e6bbeb04f512ba8444dd
Aidan Marlin <spam@gmail.com>
2022-06-18 21:38:40 +0100

    blueboy bugfix

--- hosts/blueboy/syncthing.nix
+++ hosts/blueboy/syncthing.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }: {
   services.syncthing = {
     enable = true;
+    dataDir = "/home/user/syncthing";
     guiAddress = "0.0.0.0:8384";
     extraOptions = {
       gui = {
