f52761d347fd2a997788c8f6a4cc02128b2a7620
Aidan Marlin <spam@gmail.com>
2022-06-20 16:43:36 +0100

    agenix tests

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,12 @@
 let
-  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOOZSriO6sNxrA55MGpuL/rg1Uej0m+/S76pa+Iw2AT7";
-  users = [ user ];
+  # Don't forget to `a-agenix --rekey` when updating public keys
+  user_rig = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOOZSriO6sNxrA55MGpuL/rg1Uej0m+/S76pa+Iw2AT7";
+  user_blueboy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAIHvP/ols9rRoSXH8/vxZ8Gn5WeuuYLDaDV5SKgJoQT";
+  user_rog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/bKOQ4P5Rz8qZciHunVhpcnX1lwyogNhE1YUqidM44";
+  users = [ user_rig user_blueboy user_rog ];
 in
 {
-  "secret.age".publicKeys = [ user ];
-  "wireguard-key.age".publicKeys = [ user ];
-  "wireguard-password.age".publicKeys = [ user ];
+  "secret.age".publicKeys = users;
+  "wireguard-key.age".publicKeys = users;
+  "wireguard-password.age".publicKeys = users;
 }
