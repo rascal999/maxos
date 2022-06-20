7330b9f2ec1155cdcccc8ca0b6a9b8202e5803ef
Aidan Marlin <spam@gmail.com>
2022-06-20 18:28:47 +0100

    ddclient test

--- hosts/blueboy/ddclient.nix
+++ hosts/blueboy/ddclient.nix
@@ -8,7 +8,7 @@
   services.ddclient = {
     enable = true;
     domains = [ "alm.gg" ];
-    passwordFile = config.age.secrets.ddclient-password.path;
+    passwordFile = "/tmp/tmp"; #config.age.secrets.ddclient-password.path;
     protocol = "namecheap";
     server = "dynamicdns.park-your-domain.com";
     use = "web, web=dynamicdns.park-your-domain.com/getip";
