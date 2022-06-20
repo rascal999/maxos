b16c3eb66bf3983e90d346cc2a6f90df3f87a6ff
Aidan Marlin <spam@gmail.com>
2022-06-20 16:55:00 +0100

    agenix tests

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,12 @@
 let
   # Don't forget to `a-agenix --rekey` when updating public keys
-  user_rig = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFujWqx7S6oMZf8G4SvP3+LkKjxD9ZwyCBJVqmtUl/x7";
-  user_blueboy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfxGYgiWarGdkmG3K+A3y/QR7vWVqddmEOscrory7Vf";
-  user_rog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOpZ0o7uDjhBM2zTfrH8O964mQcqVVjqEckbLr1r2Jg";
-  users = [ user_rig user_blueboy user_rog ];
+  host_rig = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFujWqx7S6oMZf8G4SvP3+LkKjxD9ZwyCBJVqmtUl/x7";
+  host_blueboy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfxGYgiWarGdkmG3K+A3y/QR7vWVqddmEOscrory7Vf";
+  host_rog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOpZ0o7uDjhBM2zTfrH8O964mQcqVVjqEckbLr1r2Jg";
+  hosts = [ host_rig host_blueboy host_rog ];
 in
 {
-  "secret.age".publicKeys = users;
-  "wireguard-key.age".publicKeys = users;
-  "wireguard-password.age".publicKeys = users;
+  "secret.age".publicKeys = hosts;
+  "wireguard-key.age".publicKeys = hosts;
+  "wireguard-password.age".publicKeys = hosts;
 }
