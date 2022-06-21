d5f02fde0bf0da2c5c56bbc1af105c1b82de9c57
Aidan Marlin <spam@gmail.com>
2022-06-21 10:38:00 +0100

    grub

--- common/shared.nix
+++ common/shared.nix
@@ -13,8 +13,6 @@ let
   ]; 
   python-with-my-packages = python3.withPackages my-python-packages;
 in {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
