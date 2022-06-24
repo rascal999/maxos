12cdf124c7e5ef65b4343e2606546304b47c2015
Aidan Marlin <spam@gmail.com>
2022-06-24 10:33:34 +0100

    Bugfix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,10 @@
 let
   # Don't forget to `a-agenix --rekey` when updating public keys
   host_blueboy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfxGYgiWarGdkmG3K+A3y/QR7vWVqddmEOscrory7Vf";
-  host_galaxy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICwkCO/1MYtq9abVhepFnm4c3D9TpCv4Mz8yhuMw+ym0";
+  host_galaxy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP4ocz0cS0v/JmgnyvqfMMYGaaAYflJCy2AJthWA6Met";
   host_rig = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFujWqx7S6oMZf8G4SvP3+LkKjxD9ZwyCBJVqmtUl/x7";
   host_rog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOpZ0o7uDjhBM2zTfrH8O964mQcqVVjqEckbLr1r2Jg";
-  hosts = [ host_rig host_blueboy host_rog ];
+  hosts = [ host_blueboy host_galaxy host_rig host_rog ];
 in
 {
   "ddclient-password.age".publicKeys = hosts;
