9394d46567ffe8593a891103a58559fdf09f5372
Aidan Marlin <spam@gmail.com>
2022-06-24 10:04:04 +0100

    Bugfix

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,5 @@
 /run/current-system/sw/bin/git clone https://github.com/rascal999/nixos ${HOME}/git/nixos || /run/current-system/sw/bin/git -C ${HOME}/git/nixos pull
 mkdir -p ${HOME}/jupyter/pentest/base
 rm ${HOME}/jupyter/pentest/base/*.ipynb
+rm -rf ${HOME}/jupyter/pentest/base/tools
 cp ${HOME}/git/nixos/resources/jupyter/pentest/*.ipynb ${HOME}/jupyter/pentest/base
