f8be87ea957475cf4c5f19335d24aff46bbb1812
Aidan Marlin <spam@gmail.com>
2022-06-27 14:50:44 +0100

    Notify system update

--- scripts/telegram_notify.sh
+++ scripts/telegram_notify.sh
@@ -14,7 +14,13 @@ DISABLE_NOTIFACITON="false"
 JSON='{"chat_id": "'$CHAT_ID'", "parse_mode":"MarkdownV2", "text": "```text
 '$HOSTNAME' | '$MESSAGE'```", "disable_notification": '$DISABLE_NOTIFICATION'}'
 
-curl -X POST \
+curl -s -X POST \
      -H 'Content-Type: application/json' \
      -d "$JSON" \
-     https://api.telegram.org/bot${BOT_API_KEY}/sendMessage
+     https://api.telegram.org/bot${BOT_API_KEY}/sendMessage > /dev/null
+
+if [[ "$?" == "0" ]]; then
+  echo "Notified via Telegram."
+else
+  echo "ERROR: Telegram notify issue."
+fi
