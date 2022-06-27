7e134449124e18330327b67fe9e3932684c47bb8
Aidan Marlin <spam@gmail.com>
2022-06-27 13:58:54 +0100

    Notify formatting

--- scripts/telegram_notify.sh
+++ scripts/telegram_notify.sh
@@ -14,5 +14,5 @@ DISABLE_NOTIFACITON="false"
 
 curl -X POST \
      -H 'Content-Type: application/json' \
-     -d "{\"chat_id\": \"$CHAT_ID\", \"text\": \"$MESSAGE\", \"disable_notification\": $DISABLE_NOTIFICATION}'" \
+     -d "{\"chat_id\": \"$CHAT_ID\", \"parse_mode\":\"MarkdownV2\", \"text\": \"\`\`\`$MESSAGE\`\`\`\", \"disable_notification\": $DISABLE_NOTIFICATION}'" \
      https://api.telegram.org/bot${BOT_API_KEY}/sendMessage
