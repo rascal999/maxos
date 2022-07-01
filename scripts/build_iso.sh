72e94fc2e0f0f451fa0a73e939eef5c97a7557d2
Aidan Marlin <spam@gmail.com>
2022-07-01 09:43:11 +0100

    build_iso.sh changes

--- scripts/build_iso.sh
+++ scripts/build_iso.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
-nix --extra-experimental-features flakes --extra-experimental-features nix-command build .#nixosConfigurations.iso.config.system.build.isoImage --impure
-/home/user/git/nixos/scripts/telegram_notify.sh -a -m "Finished building ISO."
+GIT_CURRENT_HASH=`git rev-parse --short HEAD`
+GIT_PREV_HASH=0
+
+if [[ -f "/tmp/iso_hash" ]]; then
+  GIT_PREV_HASH=`cat /tmp/iso_hash`
+fi
+
+# Check we haven't built ISO for this hash
+if [[ "$GIT_CURRENT_HASH" != "$GIT_PREV_HASH" ]]; then
+  echo "Should build"
+  #nix --extra-experimental-features flakes --extra-experimental-features nix-command build .#nixosConfigurations.iso.config.system.build.isoImage --impure
+  # Problem?
+  if [[ "$?" == "0" ]]; then
+    /home/user/git/nixos/scripts/telegram_notify.sh -m "Finished building ISO."
+  else
+    /home/user/git/nixos/scripts/telegram_notify.sh -m "Error code while building ISO: $?"
+  fi
+else
+  /home/user/git/nixos/scripts/telegram_notify.sh -m "ISO already built for $GIT_CURRENT_HASH"
+fi
+
+echo $GIT_CURRENT_HASH > /tmp/iso_hash
