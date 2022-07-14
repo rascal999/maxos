2ef9675027ea97462abd07294624a7e2247f5b82
Aidan <c8cbfaf0-d289-4a7a-a995-68847a5e6305@gmail.com>
2022-07-14 16:40:22 +0100

    Jupyter updates

--- common/pkgs_base.nix
+++ common/pkgs_base.nix
@@ -5,8 +5,8 @@ let
   my-python-packages = python-packages: with python-packages; [
     distro
     pip
-    setuptools
     requests
+    setuptools
   ];
   python-with-my-packages = python3.withPackages my-python-packages;
 in {
