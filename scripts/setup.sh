7f664107a40b583e0a4ce86ca98f64db0404c718
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2022-07-18 13:35:21 +0100

    BugfiX

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,11 +8,11 @@ fi
 nixos-generate-config --root /mnt
 rm /mnt/etc/nixos/configuration.nix
 
+mkdir hosts/$1
+cp /mnt/etc/nixos/hardware-configuration.nix hosts/$1/
+
 # Git repo?
 if [[ -d ".git" ]]; then
-  mkdir hosts/$1
-  cp /mnt/etc/nixos/hardware-configuration.nix hosts/$1/
-
   git config --global user.email "${1}@dev.net"
   git config --global user.name "Aidan"
   git add .
