0bde6ae64f059dc7c76672279e83f56bc11b86e1
Aidan <c8cbfaf0-d289-4a7a-a995-68847a5e6305@gmail.com>
2022-07-19 15:12:12 +0100

    rog new SSH key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   host_blueboy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfxGYgiWarGdkmG3K+A3y/QR7vWVqddmEOscrory7Vf";
   host_galaxy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP4ocz0cS0v/JmgnyvqfMMYGaaAYflJCy2AJthWA6Met";
   host_rig = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3W0Fth1UvZEqaPYM+fKPn4ZStvrk8GswTSj5He3Z3d";
-  host_rog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFujWqx7S6oMZf8G4SvP3+LkKjxD9ZwyCBJVqmtUl/x7";
+  host_rog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJzSkIhetIXA8SWm95Ewq10rqZvEYr+bwrjv2AWskORL";
   hosts = [ host_blueboy host_galaxy host_rig host_rog ];
 in
 {
