5c8ecb16abd9d2ec7bd02a876ee6bde52b3c46e8
Aidan <c8cbfaf0-d289-4a7a-a995-68847a5e6305@gmail.com>
2022-07-21 10:35:38 +0100

    Bugfix

--- scripts/resources.sh
+++ scripts/resources.sh
@@ -515,22 +515,13 @@ function pull_tool_repos() {
   git_update https://github.com/opencve/opencve-docker.git $HOME/git/exploits/opencve-docker
   if [[ "$?" == "0" ]]; then
     cp $HOME/git/nixos-next/resources/configs/opencve/opencve.cfg conf
-    # sed ports
-    sed -i 's/8000/10030/g' docker-compose.yml
-    sed -i 's/${OPENCVE_PORT:-10030}:10030/127.0.0.1:10030:10030/g' docker-compose.yml
-    # sed redis/postgres image for sharelatex and ports
-    sed -i 's/redis/redis-opencve/g' docker-compose.yml
-    sed -i 's/image: redis-opencve:buster/image: redis:buster/g' docker-compose.yml
-    sed -i 's/postgres/postgres-opencve/g' docker-compose.yml
-    sed -i 's/image: postgres-opencve:11/image: postgres:11/g' docker-compose.yml
-    sed -i 's/${REDIS_PORT:-6379}:6379/6380:6380/g' docker-compose.yml
-    #sed -i 's/${POSTGRES_PORT:-5432}:5432/5433:5432/g' docker-compose.yml
+    cp $HOME/git/nixos-next/resources/configs/opencve/docker-compose.yaml .
 
     # Build and spin up
     docker-compose build
     docker-compose up -d postgres-opencve redis-opencve webserver celery_worker
     docker exec -it webserver opencve upgrade-db
-    docker exec -it webserver opencve import-data
+    docker exec -it webserver opencve import-data --confirm
   fi
 
   # redgo
