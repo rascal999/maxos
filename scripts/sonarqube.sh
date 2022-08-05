5fc17df955355f528a76cf218ad5ee1f4ca525ac
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2022-08-05 09:24:56 +0100

    Firefox selenium addon

---
+++ scripts/sonarqube.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Check if sonarqube already running
+
+# Spin up server (change port)
+docker run --rm -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:latest
+
+# Register
+
+# Create project
+
+# Get keys..
+
+# Run scan
+
+# Get results
+
+# Make results nice?
+
+# Kill SQ?
