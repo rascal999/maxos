5cffe8a2da01ddf62cb4968d660a451c4eec39b1
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2022-08-08 11:09:57 +0100

    vim updates

--- config/vimrc.nix
+++ config/vimrc.nix
@@ -4,3 +4,5 @@ set tabstop=2
 set shiftwidth=2
 set expandtab
 set magic
+autocmd FileType python map <buffer> <F5> :w<CR>:exec '!python3' shellescape(@%, 1)<CR>
+autocmd FileType python imap <buffer> <F5> <esc>:w<CR>:exec '!python3' shellescape(@%, 1)<CR>
