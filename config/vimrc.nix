16ba87b45d643903ac69f6bca06d823aceb4fe39
Aidan <c8cbfaf0-d289-4a7a-a995-68847a5e6305@gmail.com>
2022-09-29 13:54:50 +0100

    vim updates

--- config/vimrc.nix
+++ config/vimrc.nix
@@ -6,3 +6,4 @@ set expandtab
 set magic
 autocmd FileType python map <buffer> <F5> :w<CR>:exec '!python3' shellescape(@%, 1)<CR>
 autocmd FileType python imap <buffer> <F5> <esc>:w<CR>:exec '!python3' shellescape(@%, 1)<CR>
+set viminfo='20,<1000
