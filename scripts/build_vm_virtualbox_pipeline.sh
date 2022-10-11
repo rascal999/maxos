5873ddf9103cd3c7bf773886f305cd30c00e8f9b
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2022-10-11 15:33:11 +0100

    Bugfix

--- scripts/build_vm_virtualbox_pipeline.sh
+++ scripts/build_vm_virtualbox_pipeline.sh
@@ -5,4 +5,5 @@ GIT_CURRENT_HASH=`sudo -u user git -C /home/user/git/maxos rev-parse --short HEA
 # build
 /home/user/git/maxos/scripts/build_vm_virtualbox.sh && \
 rsync -acv /home/user/git/maxos/maxos_virtualbox/maxos.ova user@alm.gg:/home/user/alm.gg/www/maxos_kde.ova && \
+ssh user@alm.gg touch /home/user/alm.gg/www/maxos_kde.ova && \
 /home/user/git/maxos/scripts/telegram_notify.sh -m "VirtualBox VM built and pushed to https://alm.gg for $GIT_CURRENT_HASH"
