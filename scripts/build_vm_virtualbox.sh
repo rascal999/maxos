0d30f29315c50cde44fe298818f7ca0f16ba23ea
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2022-10-14 10:50:19 +0100

    Bugfix

--- scripts/build_vm_virtualbox.sh
+++ scripts/build_vm_virtualbox.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+usage() {
+  echo "Build VirtualBox VM script"
+  echo "Usage: $0 -m <message> [-a] [-m] [-q]" 1>&2;
+  echo
+  echo "-n (Notify)           Notify on build"
+  echo
+  echo "Examples:"
+  echo "# Do not notify"
+  echo "$0 -n"
+  echo
+  exit 1;
+}
+
+arg_notify="true"
+
+while getopts am:q flag
+do
+    case "${flag}" in
+        n) arg_notify="false";;
+    esac
+done
+
 sudo chmod 777 /tmp/vm_virtualbox_hash
 
 GIT_CURRENT_HASH=`sudo -u user git -C /home/user/git/maxos rev-parse --short HEAD`
@@ -14,14 +36,21 @@ if [[ "$GIT_CURRENT_HASH" != "$GIT_PREV_HASH" ]]; then
   nixos-generate --flake .#vm_virtualbox -f virtualbox -o maxos_virtualbox
   # Problem?
   if [[ "$?" == "0" ]]; then
-    /home/user/git/maxos/scripts/telegram_notify.sh -m "Finished building VirtualBox VM."
+    if [[ "$arg_notify" == "true" ]]; then
+      /home/user/git/maxos/scripts/telegram_notify.sh -m "Finished building VirtualBox VM."
+    fi
+
     echo $GIT_CURRENT_HASH > /tmp/vm_virtualbox_hash
     exit 0
   else
-    /home/user/git/maxos/scripts/telegram_notify.sh -m "Error code while building VirtualBox VM: $?"
+    if [[ "$arg_notify" == "true" ]]; then
+      /home/user/git/maxos/scripts/telegram_notify.sh -m "Error code while building VirtualBox VM: $?"
+    fi
     exit 1
   fi
 else
-  /home/user/git/maxos/scripts/telegram_notify.sh -m "VirtualBox VM already built for $GIT_CURRENT_HASH"
+  if [[ "$arg_notify" == "true" ]]; then
+    /home/user/git/maxos/scripts/telegram_notify.sh -m "VirtualBox VM already built for $GIT_CURRENT_HASH"
+  fi
   exit 1
 fi
--- scripts/build_vm_virtualbox_pipeline.sh
+++ scripts/build_vm_virtualbox_pipeline.sh
@@ -3,7 +3,8 @@
 GIT_CURRENT_HASH=`sudo -u user git -C /home/user/git/maxos rev-parse --short HEAD`
 
 # build
-/home/user/git/maxos/scripts/build_vm_virtualbox.sh && \
-rsync -acv /home/user/git/maxos/maxos_virtualbox/maxos.ova user@alm.gg:/home/user/alm.gg/www/maxos_kde.ova && \
+/home/user/git/maxos/scripts/build_vm_virtualbox.sh -n && \
+ssh user@alm.gg mv /home/user/alm.gg/www/maxos_kde.ova /home/user/alm.gg/www/maxos_kde_previous.ova || true && \
+rsync -av /home/user/git/maxos/maxos_virtualbox/maxos.ova user@alm.gg:/home/user/alm.gg/www/maxos_kde.ova && \
 ssh user@alm.gg touch /home/user/alm.gg/www/maxos_kde.ova && \
 /home/user/git/maxos/scripts/telegram_notify.sh -m "VirtualBox VM built and pushed to https://alm.gg for $GIT_CURRENT_HASH"
