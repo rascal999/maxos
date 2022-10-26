73b4ceb368fda6ecfeee370af5a0d83e5e72eeae
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2022-10-26 17:32:30 +0100

    Bug fix

--- scripts/build_vm_virtualbox_pipeline.sh
+++ scripts/build_vm_virtualbox_pipeline.sh
@@ -3,7 +3,7 @@
 GIT_CURRENT_HASH=`sudo -u user git -C /home/user/git/maxos rev-parse --short HEAD`
 
 # build
-/home/user/git/maxos/scripts/build_vm_virtualbox.sh -n && \
+/home/user/git/maxos/scripts/build_vm_virtualbox.sh -c vm_virtualbox -n && \
 ssh user@alm.gg mv /home/user/alm.gg/www/maxos_kde.ova /home/user/alm.gg/www/maxos_kde_previous.ova || true && \
 rsync -av /home/user/git/maxos/maxos_virtualbox/maxos.ova user@alm.gg:/home/user/alm.gg/www/maxos_kde.ova && \
 ssh user@alm.gg touch /home/user/alm.gg/www/maxos_kde.ova && \
