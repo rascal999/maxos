96d37846bd95c1187fa359b91013ade29f9c3485
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2022-10-26 13:19:26 +0100

    Allow unfree

--- config/shared.nix
+++ config/shared.nix
@@ -170,6 +170,9 @@
   services.logind.lidSwitch = "ignore";
 
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "python2.7-pyjwt-1.7.1"
+  ];
 
   # docker
   virtualisation.docker = {
