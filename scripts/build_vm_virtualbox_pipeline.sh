bbad7ef5b2f8dea8e5842a2e0138fbadfc432e2b
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2023-01-11 14:41:44 +0000

    Bug fixes

--- scripts/build_vm_virtualbox_pipeline.sh
+++ scripts/build_vm_virtualbox_pipeline.sh
@@ -4,7 +4,7 @@ GIT_CURRENT_HASH=`sudo -u user git -C /home/user/git/maxos rev-parse --short HEA
 
 # build
 /home/user/git/maxos/scripts/build_vm_virtualbox.sh -c vm_virtualbox -n && \
-ssh user@alm.gg mv /home/user/alm.gg/www/maxos_kde.ova /home/user/alm.gg/www/maxos_kde_previous.ova || true && \
-rsync -av /home/user/git/maxos/maxos_virtualbox/maxos.ova user@alm.gg:/home/user/alm.gg/www/maxos_kde.ova && \
-ssh user@alm.gg touch /home/user/alm.gg/www/maxos_kde.ova && \
+ssh root@alm.gg sudo mv /home/user/alm.gg/www/maxos_kde.ova /home/user/alm.gg/www/maxos_kde_previous.ova || true && \
+rsync --progress -av /home/user/git/maxos/maxos_vm_virtualbox/maxos.ova root@alm.gg:/home/user/alm.gg/www/maxos_kde.ova && \
+ssh root@alm.gg touch /home/user/alm.gg/www/maxos_kde.ova && \
 /home/user/git/maxos/scripts/telegram_notify.sh -m "VirtualBox VM built and pushed to https://alm.gg for $GIT_CURRENT_HASH"
