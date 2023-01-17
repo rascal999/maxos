295fe4dbffe33bfa5ba4f1c5767838d16791e428
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2023-01-17 09:38:43 +0000

    i3 updates

--- config/i3.nix
+++ config/i3.nix
@@ -19,7 +19,7 @@ in {
       for_window [instance=".*microsoft teams.*"] move to workspace "mst"
       for_window [title=".*Online LaTeX Editor Overleaf Community Edition.*"] move to workspace "rep"
       #for_window [title=".*YouTube . Mozilla Firefox.*"] move to workspace "yt"
-      #for_window [title=".*YouTube Music.*"] move to workspace "ytm"
+      for_window [title=".*YouTube Music.*"] move to workspace "ytm"
       no_focus [class="org.remmina.Remmina"]
       focus_on_window_activation none
     '';
