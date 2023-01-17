60c6721eb5ab3ea7030beef3e9abceb5f3c7cde0
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2023-01-17 09:43:06 +0000

    i3 updates

--- config/i3.nix
+++ config/i3.nix
@@ -83,7 +83,7 @@ in {
         # Workspace
         "${mod}+0" = "workspace number 10";
         "${mod}+1" = "workspace number 1";
-        "${mod}+2" = "workspace number 2";
+        "${mod}+2" = "workspace ls";
         "${mod}+3" = "workspace number 3";
         "${mod}+4" = "workspace number 4";
         "${mod}+5" = "workspace number 5";
@@ -93,7 +93,7 @@ in {
         "${mod}+9" = "workspace number 9";
         "${mod}+Shift+0" = "move container to workspace number 10";
         "${mod}+Shift+1" = "move container to workspace number 1";
-        "${mod}+Shift+2" = "move container to workspace number 2";
+        "${mod}+Shift+2" = "move container to workspace ls";
         "${mod}+Shift+3" = "move container to workspace number 3";
         "${mod}+Shift+4" = "move container to workspace number 4";
         "${mod}+Shift+5" = "move container to workspace number 5";
