7c64e6b0d933d2d5165046080b461174a8d8e439
Aidan Marlin <0ef4bbef-aacf-444c-ba58-bfa18b621f9d@gmail.com>
2023-01-24 12:03:28 +0000

    tmux and syncthing changes

--- config/syncthing.nix
+++ config/syncthing.nix
@@ -3,26 +3,26 @@
     enable = true;
 
     folders = {
-      "Camera" = {
-        id = "gm1920_16jh-photos";
+      "Phone" = {
+        id = "phone";
         devices = [
           "blueboy"
-          "fold4"
-          "GM1920"
+          "nas"
           "rig"
-          "rog"
+          #"fold4"
+          #"GM1920"
         ];
-        path = "/home/user/Camera";
+        path = "/home/user/Phone";
       };
 
       "Data" = {
-        id = "kwfru-wgejw";
+        id = "data";
         devices = [
           "blueboy"
-          "fold4"
-          "GM1920"
+          "nas"
           "rig"
-          "rog"
+          #"fold4"
+          #"GM1920"
         ];
         path = "/home/user/Data";
       };
@@ -30,33 +30,32 @@
 
     devices = {
       "blueboy" = {
-        autoAcceptFolders = true;
+        #autoAcceptFolders = true;
         id = "3YBMQYS-46IZSTQ-HLIP3O2-E22P3GJ-OS2PFCY-GTINZAF-PMH6HTN-2GDV4Q3";
         introducer = false;
       };
-      "galaxy" = {
-        autoAcceptFolders = true;
-        id = "DPWLINN-X2GRAYG-2IYFOXT-CRWLY3L-BMRDJGT-KE5ELWF-M76VOXR-XFREWQG";
-        introducer = false;
-      };
-      "fold4" = {
-        id = "PLY3QVN-3NQYXXB-PJO6KJ3-3MJ3H6B-QVQKRWI-CNX6PNS-VDADBY2-2J4VFA5";
-        introducer = false;
-      };
-      "GM1920" = {
-        id = "UAULTZS-JD7M5HI-E2BDK34-KOVLIGV-JZO357W-I2GGN25-IKOFLB7-B5SGRQD";
+      "nas" = {
+        id = "DCL3E6N-GIZIFEM-Z4ADAKR-IY2WT5M-G4ADVCI-LANSWO5-VIVH4H4-JDF6WAI";
         introducer = true;
       };
       "rig" = {
-        autoAcceptFolders = true;
+        #autoAcceptFolders = true;
         id = "3CETFIS-4P5BTDB-TQRFO54-H6ECZH4-OLHU2LB-DTQFTVA-ZFMPYJH-7FVSRA6";
         introducer = false;
       };
-      "rog" = {
-        autoAcceptFolders = true;
-        id = "TTM76HX-M6H5JG7-KZ2YHMG-UPT5HQZ-OEPQFNX-FOMAWGB-GPTYPG6-QS7DHA4";
-        introducer = false;
-      };
+      #"galaxy" = {
+      #  autoAcceptFolders = true;
+      #  id = "DPWLINN-X2GRAYG-2IYFOXT-CRWLY3L-BMRDJGT-KE5ELWF-M76VOXR-XFREWQG";
+      #  introducer = false;
+      #};
+      #"fold4" = {
+      #  id = "PLY3QVN-3NQYXXB-PJO6KJ3-3MJ3H6B-QVQKRWI-CNX6PNS-VDADBY2-2J4VFA5";
+      #  introducer = false;
+      #};
+      #"GM1920" = {
+      #  id = "UAULTZS-JD7M5HI-E2BDK34-KOVLIGV-JZO357W-I2GGN25-IKOFLB7-B5SGRQD";
+      #  introducer = false;
+      #};
     };
 
     dataDir = "/home/user/syncthing";
--- config/tmux.nix
+++ config/tmux.nix
@@ -5,12 +5,13 @@
     historyLimit = 100000;
 
     extraConfig = ''
-      bind "Enter" send-keys "exa --long --all --header --icons --git" \; send-keys "Enter"
-      bind "Space" send-keys "exa --long --all --header --icons --git --sort=modified" \; send-keys "Enter"
       bind "e" send-keys "exit" \; send-keys "Enter"
-      bind l send-keys "duf" \; send-keys "Enter"
-      bind r send-keys "uname -a" \; send-keys "Enter"
-      bind u send-keys "dust ." \; send-keys "Enter"
+      bind "Enter" send-keys "exa --long --all --header --icons --git" \; send-keys "Enter"
+      bind "l" send-keys "duf" \; send-keys "Enter"
+      bind "r" send-keys "uname -a" \; send-keys "Enter"
+      bind "Space" send-keys "exa --long --all --header --icons --git --sort=modified" \; send-keys "Enter"
+      bind "Tab" send-keys "ls -alh" \; send-keys "Enter"
+      bind "u" send-keys "dust ." \; send-keys "Enter"
 
       unbind C-b
       unbind [
