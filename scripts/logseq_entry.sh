0583476f457767217d76434853573af0680cb3c2
Aidan Marlin <aidan.marlin@gmail.com>
2023-08-24 11:43:51 +0100

    Bug fixes

--- scripts/logseq_entry.sh
+++ scripts/logseq_entry.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/env bash
 
+DELAY=400
+
 xdotool key Alt+2
+echo "Alt+2 keys"
+
 WID=`xdotool search --onlyvisible --classname logseq`
 xdotool windowfocus --sync $WID
-xdotool key --delay 150 Escape
-xdotool key --delay 150 Escape
-xdotool key --delay 150 "g"
-xdotool key --delay 150 "j"
-xdotool key --delay 150 Down
-xdotool key --delay 150 T
-xdotool key --delay 150 O
+
+xdotool key --delay $DELAY Escape
+echo "Escape key"
+
+xdotool key --delay $DELAY Escape
+echo "Escape key"
+
+xdotool key --delay $DELAY "g"
+echo "Pressed g"
+
+xdotool key --delay $DELAY "j"
+echo "Pressed j"
+
+xdotool key --delay $DELAY Down
+echo "Down key"
+
+xdotool key --delay $DELAY T
+echo "Pressed T"
+
+xdotool key --delay $DELAY O
+echo "Pressed O"
+
+sleep 0.2
+
 TIME=`date +"%H:%M"`
-xdotool type --delay 150 "$TIME"
-xdotool key --delay 150 Enter
-xdotool key --delay 150 Tab
+xdotool type --delay $DELAY "$TIME"
+echo $TIME
+
+xdotool key --delay $DELAY Enter
+echo "Enter key"
+
+xdotool key --delay $DELAY Tab
+echo "Tab key"
