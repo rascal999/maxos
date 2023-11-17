52d0360ea240598143feb96175cbc074ef37af60
Aidan Marlin <aidan.marlin@gmail.com>
2023-11-17 10:56:07 +0000

    UI improvement

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -43,7 +43,7 @@ if [[ `hostname` == "rig" ]]; then
     /run/current-system/sw/bin/sleep 0.2
     /run/current-system/sw/bin/daemon /run/current-system/sw/bin/slack
     /run/current-system/sw/bin/sleep 0.2
-    /run/current-system/sw/bin/daemon -X "/etc/profiles/per-user/user/bin/chromium --force-device-scale-factor=1.6 --new-window https://web.whatsapp.com/"
+    /run/current-system/sw/bin/daemon -X "/etc/profiles/per-user/user/bin/chromium --force-device-scale-factor=1.6 --new-window --app=https://web.whatsapp.com/"
     /run/current-system/sw/bin/sleep 0.2
     /run/current-system/sw/bin/daemon -X "/run/current-system/sw/bin/mpv https://d1mpprlbe8tn2j.cloudfront.net/v1/master/7b67fbda7ab859400a821e9aa0deda20ab7ca3d2/euronewsLive/87O7AhxRUdeeIVqf/ewnsabren_eng.m3u8"
     /run/current-system/sw/bin/sleep 0.2
