c60b9780818c814a85f81f5b0d8d56873f786813
Aidan Marlin <aidan.marlin@gmail.com>
2023-11-20 15:39:59 +0000

    Postman

--- config/pkgs_ui.nix
+++ config/pkgs_ui.nix
@@ -13,6 +13,7 @@
     lxappearance                                                                                         # Lightweight program for configuring the theme and fonts of gtk applications
     networkmanager_dmenu                                                                                 #
     notify-desktop                                                                                       # Little application that lets you send desktop notifications with one command
+    postman
     redshift                                                                                             # Screen color temperature manager
     slack                                                                                                # Desktop client for Slack
     tesseract                                                                                            # OCR engine
