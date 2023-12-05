35899e3311d39a58e6818a025881805860a234fb
Aidan Marlin <aidan.marlin@gmail.com>
2023-12-05 12:56:02 +0000

    Bug fix

--- config/shared.nix
+++ config/shared.nix
@@ -188,9 +188,6 @@
   services.k3s.enable = false;
   #services.k3s.role = "server";
 
-  # Enable twingate
-  services.twingate.enable = true;
-
   # Ignore lid on laptops
   services.logind.lidSwitch = "ignore";
 
