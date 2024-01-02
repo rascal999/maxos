6f020bcb9dc2486dfa4b035bbba27291dbdfe454
Aidan Marlin <aidan.marlin@gmail.com>
2024-01-02 11:09:57 +0000

    Bug fix

--- config/i3.nix
+++ config/i3.nix
@@ -120,7 +120,7 @@ in {
 
       bars = [
         {
-          trayOutput = "HDMI-0";
+          trayOutput = "DP-4";
           fonts = [ "DejaVu Sans Mono" "FontAwesome 14" ];
           position = "bottom";
           statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${./i3status-rust.toml}";
