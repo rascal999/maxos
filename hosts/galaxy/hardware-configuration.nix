7f4dee22367d641dac42c41a55af136995def6cc
Aidan Marlin <aidan.marlin@gmail.com>
2024-01-02 15:34:12 +0000

    Bug fix

--- hosts/galaxy/hardware-configuration.nix
+++ hosts/galaxy/hardware-configuration.nix
@@ -19,12 +19,12 @@
     };
 
   fileSystems."/home" =
-    { device = "/dev/mapper/home";
+    { device = "/dev/disk/by-uuid/5b5a26fd-902b-463c-ab8c-680cbbcc478b";
       fsType = "ext4";
     };
 
   boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/b9057238-1bdc-4163-b894-f9fcd7d36268";
-  boot.initrd.luks.devices."home".device = "/dev/disk/by-id/nvme-Samsung_SSD_990_PRO_2TB_2023031000014";
+  boot.initrd.luks.devices."home".device = "/dev/disk/by-uuid/754e3e99-f70c-40a3-968f-fb13d021aca0";
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/C64E-7617";
