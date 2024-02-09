3b23f96558143dfa1a60f9ff6d8076b0bb27b995
Aidan Marlin <aidan.marlin@gmail.com>
2024-02-09 15:23:48 +0000

    click.sh

--- scripts/click.sh
+++ scripts/click.sh
@@ -7,54 +7,86 @@ if ! command -v xdotool &> /dev/null; then
 fi
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <workspace> <label>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <action> <workspace> <label>"
     exit 1
 fi
 
-# Move to correct workspace
-/etc/profiles/per-user/user/bin/i3-msg "workspace $1"
+function slack() {
+    case "$3" in
+        "slack_jeremy")
+            /run/current-system/sw/bin/xdg-open "slack://channel?team=T02ER9ABV&id=U04GGBDUWH0"
+            ;;
+        "slack_pierre")
+            /run/current-system/sw/bin/xdg-open "slack://channel?team=T02ER9ABV&id=U06G9SFKAJ2"
+            ;;
+        "slack_security_squad")
+            /run/current-system/sw/bin/xdg-open "slack://channel?team=T02ER9ABV&id=C050ZTPBV4H"
+            ;;
+        "slack_mathieu")
+            /run/current-system/sw/bin/xdg-open "slack://channel?team=T02ER9ABV&id=U05C566PDJ5"
+            ;;
+        # Add more label mappings as needed
+        *)
+            echo "Invalid label."
+            exit 1
+            ;;
+    esac
+}
 
-TILE_COUNT=`/etc/profiles/per-user/user/bin/i3-save-tree | grep class | wc -l`
+function click() {
+    # Move to correct workspace
+    /etc/profiles/per-user/user/bin/i3-msg "workspace $2"
 
-y_coord=290
-# Define label-to-coordinates mappings
-case "$2" in
-    "ynab")
-        x_coord=5150
-        ;;
-    "calendar")
-        y_coord=340
-        x_coord=5190
-        ;;
-    "gpt")
-        x_coord=5340
-        ;;
-    "sprint_1")
-        y_coord=340
-        x_coord=5290
-        ;;
-    "sprint_2")
-        y_coord=340
-        x_coord=5340
-        ;;
-    "sprint_3")
-        y_coord=340
-        x_coord=5390
-        ;;
-    # Add more label mappings as needed
-    *)
-        echo "Invalid label. Supported labels: button, menu"
-        exit 1
-        ;;
-esac
+    TILE_COUNT=`/etc/profiles/per-user/user/bin/i3-save-tree | grep class | wc -l`
 
-if [[ "$TILE_COUNT" == "2" ]]; then
-    y_coord=$((y_coord+50))
+    y_coord=290
+    # Define label-to-coordinates mappings
+    case "$3" in
+        "tab_ynab")
+            x_coord=5150
+            ;;
+        "tab_calendar")
+            y_coord=340
+            x_coord=5190
+            ;;
+        "gpt")
+            x_coord=5340
+            ;;
+        "sprint_1")
+            y_coord=340
+            x_coord=5290
+            ;;
+        "sprint_2")
+            y_coord=340
+            x_coord=5340
+            ;;
+        "sprint_3")
+            y_coord=340
+            x_coord=5390
+            ;;
+        # Add more label mappings as needed
+        *)
+            echo "Invalid label."
+            exit 1
+            ;;
+    esac
+
+    if [[ "$TILE_COUNT" == "2" ]]; then
+        y_coord=$((y_coord+50))
+    fi
+
+    # Move mouse to specified coordinates
+    xdotool mousemove "$x_coord" "$y_coord"
+
+    # Simulate a left mouse button click
+    #xdotool click 1
+}
+
+if [ "$1" == "click" ]; then
+    click $@
 fi
 
-# Move mouse to specified coordinates
-xdotool mousemove "$x_coord" "$y_coord"
-
-# Simulate a left mouse button click
-#xdotool click 1
+if [ "$1" == "slack" ]; then
+    slack $@
+fi
