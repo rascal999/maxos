b043e8e99f0927248252a9a74b793b0a7abd621c
Aidan Marlin <aidan.marlin@gmail.com>
2024-02-13 15:50:11 +0000

    X11 over SSH

--- hosts/rig/configuration.nix
+++ hosts/rig/configuration.nix
@@ -8,6 +8,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "yes";
+  services.openssh.fowardX11 = true;
 
   # Authorised keys
   users.users.user = {
