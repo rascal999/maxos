52127888961da35ca12483e90e17c55ba49c2166
Aidan Marlin <aidan.marlin@gmail.com>
2024-02-13 15:51:14 +0000

    Bug fix

--- hosts/rig/configuration.nix
+++ hosts/rig/configuration.nix
@@ -8,7 +8,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "yes";
-  program.sssh.fowardX11 = true;
+  program.ssh.fowardX11 = true;
 
   # Authorised keys
   users.users.user = {
