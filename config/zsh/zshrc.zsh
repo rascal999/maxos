d242bbf96d0ab61114c5012f3fbe4f4f6fa08580
Aidan Marlin <aidan.marlin@gmail.com>
2024-02-15 17:28:00 +0000

    Bug fix

--- config/zsh/zshrc.zsh
+++ config/zsh/zshrc.zsh
@@ -3066,12 +3066,12 @@ jira_tasks() {
   echo "Tasks:"
 
   grep -E "DONE" ./${1}.md | choose -f '- ' -1 > ./tasks_tmp.txt
-  grep -E "DOING" ./${1}.md | choose -f '- ' -1 >> ./tasks_tmp.txt
   grep -E "TODO" ./${1}.md | choose -f '- ' -1 >> ./tasks_tmp.txt
+  grep -E "DOING" ./${1}.md | choose -f '- ' -1 >> ./tasks_tmp.txt
 
   sed -i 's/DONE/\\033[1;32mDONE\\033[0m/g' ./tasks_tmp.txt
-  sed -i 's/DOING/\\033[1;33mDOING\\033[0m/g' ./tasks_tmp.txt
   sed -i 's/TODO/\\033[0;31mTODO\\033[0m/g' ./tasks_tmp.txt
+  sed -i 's/DOING/\\033[1;33mDOING\\033[0m/g' ./tasks_tmp.txt
   TASKS_TMP=$(cat ./tasks_tmp.txt)
   echo $TASKS_TMP
   rm ./tasks_tmp.txt
