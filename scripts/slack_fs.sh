9812f10ed1f7191ec79c8185919273ef1018de5d
Aidan Marlin <aidan.marlin@gmail.com>
2024-03-04 10:57:43 +0000

    Bug fix

--- scripts/slack_fs.sh
+++ scripts/slack_fs.sh
@@ -6,11 +6,12 @@ SLACK_PARKED=`i3-save-tree | grep class | grep Slack | wc -l`
 # IF in ytm
 if [[ "$SLACK_PARKED" = "1" ]]; then
   i3-msg "[class=\"Slack\"] move to workspace 1"
-  sleep 0.3
+  sleep 0.2
   i3-msg "[class=\"Slack\"] focus"
   i3-msg "[class=\"Slack\"] fullscreen"
 else
   i3-msg "[class=\"Slack\"] move to workspace ytm"
+  sleep 0.2
   i3-msg "[class=\"Slack\"] fullscreen"
   i3-msg "[workspace=\"1\"] focus"
 fi
