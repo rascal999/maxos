dcde030c2c55e10b0c3728aeb7882e2224e2312d
Aidan Marlin <aidan.marlin@gmail.com>
2024-03-05 13:09:30 +0000

    Shortcut

--- config/i3.nix
+++ config/i3.nix
@@ -127,6 +127,7 @@ in {
         "Mod4+o" = "exec /home/user/git/maxos/scripts/slack.sh T02ER9ABV U058BK3775F";
         "Mod4+t" = "exec /home/user/git/maxos/scripts/slack.sh T02ER9ABV U03SC6Q1ZLG";
         "Mod4+d" = "exec /home/user/git/maxos/scripts/slack.sh T02ER9ABV U068KQNQ341";
+        "Mod4+c" = "exec /home/user/git/maxos/scripts/slack.sh T02ER9ABV C04H5N44ZM5";
       };
 
       window = {
