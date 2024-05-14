d6d26345e1c3db5ec0dca10bc1da0052c04e729e
Aidan Marlin <aidan.marlin@gmail.com>
2024-05-14 14:52:58 +0100

    vdirsyncer

--- hosts/rig/configuration.nix
+++ hosts/rig/configuration.nix
@@ -68,6 +68,7 @@
   services.cron = {
     enable = true;
     systemCronJobs = [
+      "*/5 * * * *       user    /run/current-system/sw/bin/vdirsyncer sync"
       "45 16 * * *       user    /run/current-system/sw/bin/rsync -av --progress /home/user/Data admin@192.168.0.254:/volume1/backup-data"
       "50 16 * * *       user    /run/current-system/sw/bin/rsync -av --progress /home/user/work admin@192.168.0.254:/volume1/backup-data"
       #"25 * * * *        user    /etc/profiles/per-user/user/bin/twmnc -c '### Take a break ###'"
