32a16f0a51e8a931b4d1c46abc0551f0c6002284
Aidan Marlin <aidan.marlin at gmail.com>
2024-05-28 16:20:06 +0100

    Burp bump

--- scripts/burp.sh
+++ scripts/burp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -jar $HOME/Downloads/burpsuite_pro_v2022.2.5.jar
-java -Xmx8G --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -jar $HOME/Downloads/burpsuite_pro_v2023.12.1.4.jar
+java -Xmx8G --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -jar $HOME/Downloads/burpsuite_pro_v2024.4.4.jar
