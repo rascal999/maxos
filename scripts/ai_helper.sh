3fa520f48c5be3d1bd4188adb087454fd70f946e
Aidan Marlin <aidan.marlin at gmail.com>
2024-11-11 10:59:55 +0000

    Update

--- scripts/ai_helper.sh
+++ scripts/ai_helper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TEXT=`flameshot gui --raw | tesseract stdin stdout`
+TEXT=`flameshot gui --raw | tesseract -l eng stdin stdout`
 PROMPT="1. Summarise this chat thread in one or two sentences.
 2. Create a table with two columns, 'Reply Type', and 'Reply'.
 3. Populate table with 3-8 rows of different potential replies.
