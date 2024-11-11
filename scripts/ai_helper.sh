b63b69c98c3b32c1be59f1e9a599b8f64c48fd15
Aidan Marlin <aidan.marlin at gmail.com>
2024-11-11 10:51:39 +0000

    Updates

--- scripts/ai_helper.sh
+++ scripts/ai_helper.sh
@@ -2,10 +2,10 @@
 
 TEXT=`flameshot gui --raw | tesseract stdin stdout`
 PROMPT="1. Summarise this chat thread in one or two sentences.
-2. Create a table with two columns, 'Response Type', and 'Response'.
-3. Populate table with 3-5 rows of different responses.
+2. Create a table with two columns, 'Reply Type', and 'Reply'.
+3. Populate table with 3-8 rows of different potential replies.
 4. Don't include chat thread in response.
-5. Don't use exclamation marks in response."
+5. Don't use exclamation marks in replies."
 
 QUERY=`echo "$PROMPT\n\n$TEXT" | jq -s -R -r @uri`
 
