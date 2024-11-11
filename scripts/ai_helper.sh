10948ebfd2478c7928decad5814697e14b38c055
Aidan Marlin <aidan.marlin at gmail.com>
2024-11-11 10:56:36 +0000

    Update

--- scripts/ai_helper.sh
+++ scripts/ai_helper.sh
@@ -7,6 +7,8 @@ PROMPT="1. Summarise this chat thread in one or two sentences.
 4. Don't include chat thread in response.
 5. Don't use exclamation marks in replies."
 
-QUERY=`echo "$PROMPT\n\n$TEXT" | jq -s -R -r @uri`
+QUERY=`echo "$PROMPT
+
+$TEXT" | jq -s -R -r @uri`
 
 firefox http://localhost:3000/?q=$QUERY
