d35cff22dab764457d8f74927f922d341a59980b
Aidan Marlin <aidan.marlin at gmail.com>
2024-12-30 14:50:57 +0000

    Bug fix

--- config/secrets.nix
+++ config/secrets.nix
@@ -61,87 +61,92 @@
     api-huggingface =
     {
       source = config.age.secrets.api-huggingface.path;
-      mode = "0444";
+      mode = "0400";
     };
     api-instagram =
     {
       source = config.age.secrets.api-instagram.path;
-      mode = "0444";
+      mode = "0400";
     };
     api-nist =
     {
       source = config.age.secrets.api-nist.path;
-      mode = "0444";
+      mode = "0400";
     };
     api-shodan =
     {
       source = config.age.secrets.api-shodan.path;
-      mode = "0444";
+      mode = "0400";
     };
     api-strava =
     {
       source = config.age.secrets.api-strava.path;
-      mode = "0444";
+      mode = "0400";
     };
     api-tapo-username =
     {
       source = config.age.secrets.api-tapo-username.path;
-      mode = "0444";
+      mode = "0400";
     };
     api-tapo-password =
     {
       source = config.age.secrets.api-tapo-password.path;
-      mode = "0444";
+      mode = "0400";
     };
     api-telegram =
     {
       source = config.age.secrets.api-telegram.path;
-      mode = "0444";
+      mode = "0400";
     };
     api-virustotal =
     {
       source = config.age.secrets.api-virustotal.path;
-      mode = "0444";
+      mode = "0400";
+    };
+    backup-info =
+    {
+      source = config.age.secrets.backup-info.path;
+      mode = "0400";
     };
     credentials-backup =
     {
       source = config.age.secrets.credentials-backup.path;
-      mode = "0444";
+      mode = "0400";
     };
     credentials-google =
     {
       source = config.age.secrets.credentials-google.path;
-      mode = "0444";
+      mode = "0400";
     };
     jira =
     {
       source = config.age.secrets.jira.path;
-      mode = "0444";
+      mode = "0400";
     };
     token-github =
     {
       source = config.age.secrets.token-github.path;
-      mode = "0444";
+      mode = "0400";
     };
     vpn-mullvad =
     {
       source = config.age.secrets.vpn-mullvad.path;
-      mode = "0444";
+      mode = "0400";
     };
     wg_home_pav =
     {
       source = config.age.secrets.wireguard-pav.path;
-      mode = "0444";
+      mode = "0400";
     };
     wg_home_rig =
     {
       source = config.age.secrets.wireguard-rig.path;
-      mode = "0444";
+      mode = "0400";
     };
     wg_home_rog =
     {
       source = config.age.secrets.wireguard-rog.path;
-      mode = "0444";
+      mode = "0400";
     };
   };
 }
